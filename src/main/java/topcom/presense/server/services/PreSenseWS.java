d8c60e079cd3b7d0a247a6a5309beb88afbe2809
Vítor Tonon <vitorrtonon@gmail.com>
2015-10-14 18:00:51 -0300

    Testando WebService

---
+++ src/main/java/topcom/presense/server/services/PreSenseWS.java
@@ -0,0 +1,19 @@
+package topcom.presense.server.services;
+
+public class PreSenseWS {
+
+	public String receiveInfoFromSensor(byte[] resource) {
+
+	/* 
+			O Sensor envia as informacoes de presenca (no caso, o arquivo json) 
+		por meio deste metodo. Portanto, byte[] resource corresponde a esse 
+		arquivo (lembrando que deve vir em base64).
+			Nesse metodo, implementar a logica do que acontece no servidor quando
+		o sensor envia algo. Deve conter chamadas de outros metodos em outras
+		classes do servidor.
+			Se necessario, da para particionar esse metodo em varios outros.
+	*/
+
+		return "Testando...";
+	}
+}
